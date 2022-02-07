61525a72e8246fabdbecb35a877f25624cabe526
Jun Kakishima <adventureocean3699@gmail.com>
2022-02-07 14:54:03 +0900

    add flash message for login operation
    update main.py:
    * add flash_message to def[register] and def[login] --to show flash messages to users when they fail to register or log in

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from flask import Flask, render_template, request, url_for, redirect, flash, sen
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
+from sqlalchemy.exc import IntegrityError
 
 app = Flask(__name__)
 
@@ -45,23 +46,38 @@ def register():
             method='pbkdf2:sha256',
             salt_length=8
         )
-        new_user = User(
-            email=request.form['email'],
-            password=hash_and_salted_password,
-            name=request.form['name'],
-        )
-        db.session.add(new_user)
-        db.session.commit()
-        login_user(new_user)
-        return redirect(url_for('secrets', name=new_user.name))
+        try:
+            new_user = User(
+                email=request.form['email'],
+                password=hash_and_salted_password,
+                name=request.form['name'],
+            )
+            db.session.add(new_user)
+            db.session.commit()
+            login_user(new_user)
+            return redirect(url_for('secrets', name=new_user.name))
+        except IntegrityError:
+            flash("You've already signed up with that email, log in instead!")
+            return redirect(url_for('login'))
     return render_template("register.html")
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-        user = db.session.query(User).filter_by(email=request.form['email']).one()  # find user by input email
-        if check_password_hash(user.password, request.form['password']):  # if input password is correct, user logged in
+        user = db.session.query(User).filter_by(email=request.form['email']).first()  # find user by input email
+        password = request.form['password']
+
+        # Email doesn't exist
+        if not user:
+            flash('That email does not exist, please try again.')
+            return redirect(url_for('login'))
+        # Incorrect password
+        elif not check_password_hash(user.password, password):
+            flash('Password incorrect, please try again.')
+            return redirect(url_for('login'))
+        # Email exist and correct password
+        else:
             login_user(user)
             return redirect(url_for('secrets'))
 
